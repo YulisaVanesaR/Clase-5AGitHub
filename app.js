408b17b1ec8f6da03fc6f5990df807dd21c57478
unknown <yulisarivas31@gmail.com>
2022-09-18 03:06:30 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+Hola mundo, mi primer archivo modificado en Git Bash (touch app.js)
+Nota: Agregar archivos que quiero seguir con el comando git add .
+
+Con el comando git add agrego los archivos que quiero seguir, los cuales están listos y  preparados para ser subido
+Sin embargo, al escribir aquí será modificado , porque he usado el comando NANO (nano)
+
+Cada vez que se modifica un archivo debo agregarlo  
+Dos opciones:
+1. Agregarlo para que comience a seguirlo a partir de esa modificacion
+2.  Ó usar el comando () paea descartar los cambios  que uno hizo
+
+Lo que tengo que hacer cada vez que  modifico un archivo
+
+
